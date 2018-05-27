559da21fed28ce1183af9e1f1f18fd2122300044
Vlad Rogovskiy <rogovski.vlad@gmail.com>
2018-05-27 23:02:22 +0300

    e-v2

--- edu-mentors-pay express/routes/tests.js
+++ edu-mentors-pay express/routes/tests.js
@@ -1,74 +1,94 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 var express = require('express');
 var faker = require('faker');
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.send([
-{
-    "question": "?",
-    "answers": 
+var question1 = {
+    question: faker.random.words() + "?",
+    answers: 
         {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4"
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4"
         },
-    "correct_answer": "4"
-},
-{
-    "question": "??",
-    "answers": 
-        {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4"
-        },
-    "correct_answer": "3"
-},
-{
-    "question": "???",
-    "answers": 
-        {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4"
-        },
-    "correct_answer": "2"
-},
-{
-    "question": "????",
-    "answers": 
-        {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4"
-        },
-    "correct_answer": "2"
-},
-{
-    "question": "?????",
-    "answers": 
-        {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4"
-        },
-    "correct_answer": "1"
+    correct_answer: getRandomInt(1,4) 
 }
-]);
+var question2 = {
+    question: faker.random.words() + "?",
+    answers: 
+        {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4"
+        },
+    correct_answer: getRandomInt(1,4) 
+}
+var question3 = {
+    question: faker.random.words() + "?",
+    answers: 
+        {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4"
+        },
+    correct_answer: getRandomInt(1,4) 
+}
+var question4 = {
+    question: faker.random.words() + "?",
+    answers: 
+        {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4"
+        },
+    correct_answer: getRandomInt(1,4) 
+}
+var question5 = {
+    question: faker.random.words() + "?",
+    answers: 
+        {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4"
+        },
+    correct_answer: getRandomInt(1,4) 
+}
+
+router.get('/', function(req, res) {
+  res.send(
+    [
+        question1,
+        question2,
+        question3,
+        question4,
+        question5
+    ]
+);
 });
 
-router.get('/:id', function(req, res) {
-  res.send({ id: req.params.username,
-             question: faker.random.words(),
-             correct_answer: faker.random.number(),
-             format: req.query.details});
+router.get('/1', function(req, res) {
+  res.send(question1);
 });
-
+router.get('/2', function(req, res) {
+  res.send(question2);
+});
+router.get('/3', function(req, res) {
+  res.send(question3);
+});
+router.get('/4', function(req, res) {
+  res.send(question4);
+});
+router.get('/5', function(req, res) {
+  res.send(question5);
+});
 
 module.exports = router;
